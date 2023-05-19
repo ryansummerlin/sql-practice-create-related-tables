0b5f374dcc2a485df4dd788f09ac1b31ecf4283f
ryansummerlin <ryansummerlin777@gmail.com>
2023-05-18 19:01:28 -0700

    created a one to many and a many to many relationship

--- sql-practice/music.sql
+++ sql-practice/music.sql
@@ -11,9 +11,26 @@ CREATE TABLE bands (
 CREATE TABLE musicians (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100)
+  last_name VARCHAR(100),
+  band_id INTEGER,
+  FOREIGN KEY (band_id) REFERENCES bands(id)
 );
 CREATE TABLE instruments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   type VARCHAR(100) NOT NULL
+);
+
+
+-- insert non-existent band into musician row
+INSERT INTO musicians (first_name, last_name, band_id)
+VALUES
+  ('Ryan', 'Summerlin', 1);
+
+
+-- Create join table
+CREATE TABLE musicians_instruments (
+  musician_id INTEGER NOT NULL,
+  instruments_id INTEGER NOT NULL,
+  FOREIGN KEY (musician_id) REFERENCES musicians(id),
+  FOREIGN KEY (instruments_id) REFERENCES instruments(id)
 );
